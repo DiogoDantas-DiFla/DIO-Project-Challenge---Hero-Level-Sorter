05f2c995849fa1ed54e01c136c7bac6f949cc5dd
DiogoDantas-DiFla <dantasdiogo.dd@gmail.com>
2023-11-20 13:33:25 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# DIO Project Challenge - Simulating a Bank Account Through the Terminal/Console
+
+This repository is a "Project Challenge" of the bootcamp "Potência Tech iFood - Programação do Zero" promoted by the courses and bootcamps website Digital Innovation One (DIO) with the aim of enabling students of this bootcamp to have a solid foundation in Programming Logic.
+
+Regardless of the area of activity that a programmer will pursue, it is necessary to learn Programming Logic even if the programmer already programs in a Programming Language such as C, Java, Python, etc, since Programming Logic will reinforce its base.
+
+The idea of this challenge is to create a program, no matter the programming language (I picked Java for this challenge), where the user inserts the name and the Experience (XP) of a Hero and the program displays the name and the XP of the hero in a pop-up window.
+
+## Prerequisites for running the project
+- JDK 8+ (I'm using JDK 17)
+- IDE for Java development (I'm using IntelliJ IDEA Community Edition version 2023.3.5)
---
+++ src/HeroLevelSorter.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+
+public class HeroLevelSorter {
+
+    public static void main(String[] args) {
+        String heroName = JOptionPane.showInputDialog("What is the name of the hero?");
+        while (heroName.isBlank()) {
+            JOptionPane.showMessageDialog(null, "Please, insert a name", "Empty Field", JOptionPane.ERROR_MESSAGE);
+            heroName = JOptionPane.showInputDialog("What is the name of the hero?");
+        }
+
+        String literalXP = JOptionPane.showInputDialog("What is your XP?");
+        while (literalXP.isBlank() || !literalXP.matches("[0-9]+")) {
+            JOptionPane.showMessageDialog(null, "Please, insert a number with no letters", "Empty Field", JOptionPane.ERROR_MESSAGE);
+            literalXP = JOptionPane.showInputDialog("What is your XP?");
+        }
+
+        int numericXP = Integer.parseInt(literalXP);
+
+        if (numericXP < 1000) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Iron level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        } else if (numericXP > 1001 && numericXP < 2000) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Bronze level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        } else if (numericXP > 2001 && numericXP < 5000) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Silver level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        } else if (numericXP > 5001 && numericXP < 7000) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Gold level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        } else if (numericXP > 7001 && numericXP < 8000) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Platinum level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        } else if (numericXP > 8001 && numericXP < 9000) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Ascending level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        } else if (numericXP > 9001 && numericXP < 10000) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Imortal level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        } else if (numericXP >= 10001) {
+            JOptionPane.showMessageDialog(null, "The Hero named " + heroName + " is at Radiant level",
+                    "Hero Level", JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
+}
